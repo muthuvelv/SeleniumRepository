b6cd27cec5fd402550f0e49d2f997f54b7c1d4f6
muthuvelv <52109209+muthuvelv@users.noreply.github.com>
2023-02-12 06:13:23 +0530

    Add files via upload

---
+++ week2/day1/Assign.java
@@ -0,0 +1,49 @@
+package week2.day1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver ChDriver=new ChromeDriver();
+		
+		ChDriver.get("http://leaftaps.com/opentaps/control/main");
+		
+		ChDriver.manage().window().maximize();
+		
+		ChDriver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		ChDriver.findElement(By.id("password")).sendKeys("crmsfa");
+		ChDriver.findElement(By.className("decorativeSubmit")).click();
+		
+		
+		ChDriver.findElement(By.linkText("CRM/SFA")).click();
+		ChDriver.findElement(By.linkText("Leads")).click();
+		ChDriver.findElement(By.linkText("Create Lead")).click();
+		ChDriver.findElement(By.id("createLeadForm_companyName")).sendKeys("Test Leaf");
+		ChDriver.findElement(By.id("createLeadForm_firstName")).sendKeys("Muthuvel");
+		ChDriver.findElement(By.id("createLeadForm_lastName")).sendKeys("Velusamy");
+		ChDriver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Muthu");
+		ChDriver.findElement(By.name("departmentName")).sendKeys("Qualit Assurance");
+		ChDriver.findElement(By.name("description")).sendKeys("Automation Test Using Selenium Script");
+		ChDriver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("muthu@testleaf.com");
+		
+		WebElement stateElement = ChDriver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		
+		Select listelement=new Select(stateElement);
+		//listelement.selectByValue("New York");
+		listelement.selectByVisibleText("New York");
+		
+		ChDriver.findElement(By.name("submitButton")).click();
+		
+		
+
+	}
+
+}
